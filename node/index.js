521df754add5454b2f92dbbe9cca85ec9a0b871f
Eskaine <eskaineliu@gmail.com>
2022-11-09 21:49:56 +0800

    chore: added base route res

--- node/index.js
+++ node/index.js
@@ -4,7 +4,7 @@ const limiter = require('./configs/limiter.config');
 const connectMongo = require('./configs/mongo.config');
 const adminRouter = require('./routes/admin');
 const productsRouter = require('./routes/products');
-const cors = require('./middleware/cors');
+// const cors = require('./middleware/cors');
 const errorHandler = require('./middleware/error');
 
 const port = process.env.PORT;
@@ -14,8 +14,9 @@ connectMongo();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+// app.use(cors);
 app.use(limiter);
+app.get('/', (req, res) => res.send('API running...'));
 app.use('/admin', adminRouter);
 app.use('/products', productsRouter);
 app.use(errorHandler);
