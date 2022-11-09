476e628832e528e182ef02e492942d30ca44eece
Eskaine <eskaineliu@gmail.com>
2022-11-10 00:08:38 +0800

    fix: update cors path

--- node/middleware/cors.js
+++ node/middleware/cors.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://jaxx-coding-challenge-aujclqrl7-eskaine.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://jaxx-coding-challenge-q5c7kid1l-eskaine.vercel.app/");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
